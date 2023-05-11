8cf2ebab50a154f2f1f22264487478d7f8b972d6
wcordstudio <pidque85@gmail.com>
2023-05-12 01:00:39 +0200

    first commit

---
+++ src/js/soporte.js
@@ -0,0 +1,5 @@
+function enviar(){
+    var codigo=Math.floor(Math.random() * 9999)+1111;
+    alert("¡Has enviado el ticket correctamente!\nEl código del ticket es: #"+codigo);
+    window.location.href="https://youtube.com";
+}
